0959532fb01b4cc6fa1b0d3bb24121f1a15735f4
Rishirwt <rishabhrwt03@gmail.com>
2018-09-29 01:01:22 +0530

    second

--- app/src/main/java/com/example/dell/memoapp/splashactivity.java
+++ app/src/main/java/com/example/dell/memoapp/splashactivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class splashactivity extends AppCompatActivity {
@@ -14,7 +16,11 @@ public class splashactivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashactivity);
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
 
 
